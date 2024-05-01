5a2ad8ea626e85bc091544092756894fc3743ff6
elimkane <161225990+elimkane@users.noreply.github.com>
2024-05-01 22:55:38 +0000

    Update users.service.ts

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -11,7 +11,8 @@ import { UserModel } from './users.model';
 
 export class UserService extends UnsubscribeOnDestroyAdapter {
 
-  private readonly API_URL = 'http://localhost:8080';
+ // private readonly API_URL = 'http://localhost:8080';
+  private readonly API_URL = 'https://next-app-version-pw4zv.ondigitalocean.app';
 
   isTblLoading = true;
 
