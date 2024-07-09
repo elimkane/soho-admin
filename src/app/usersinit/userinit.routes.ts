2d6ee40f5e611d596ead6a7c7cef010d1d58d96f
elimkane <161225990+elimkane@users.noreply.github.com>
2024-07-09 02:54:02 +0000

    Create userinit.routes.ts

---
+++ src/app/usersinit/userinit.routes.ts
@@ -0,0 +1,12 @@
+import { Route } from '@angular/router';
+import { UsersComponent } from './users.component';
+
+
+
+export const USERS_ROUTE: Route[] = [
+  {
+    path: "",
+    component: UsersComponent,
+  },
+
+];
